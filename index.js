6fe483ab510f5832a61f14387e87ed9d08b3724c
Ricardo-HJ <ricardo_antoniohj@hotmail.com>
2026-09-18 19:06:49 -0500

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require(cors)
 const app = express()
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(unknownEndpoint)
 
@@ -76,6 +78,6 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
