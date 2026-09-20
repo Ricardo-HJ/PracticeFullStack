9a9e91150ddb106d6feb197622f00417c2ea4d6a
Ricardo-HJ <ricardo_antoniohj@hotmail.com>
2026-09-20 15:33:36 -0500

    refactored code

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticjs from '@stylisticjs/eslitn-plugin'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylisticjs': stylisticjs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+  {
+    ignores: ['dist/**']
+  }
+]
--- models/note.js
+++ models/note.js
@@ -1,20 +1,4 @@
-require('dotenv').config()
-
-const dns = require('dns')
-dns.setServers(['8.8.8.8', '1.1.1.1'])
-
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url, { family:4 })
-    .then(res => {
-        console.log('Connected to MongoDB')
-    })
-    .catch(err => {
-        console.log('failed to connect to MongoDB:', err.message)
-    })
 
 const noteSchema = new mongoose.Schema({
     content: {
